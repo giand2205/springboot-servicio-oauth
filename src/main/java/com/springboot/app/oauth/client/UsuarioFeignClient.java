a217f8251e87eb86d49572a990512b13131b5b8e
Gian Wong <giand2205@gmail.com>
2021-08-11 19:11:21 -0500

    configurando servicio oauth

---
+++ src/main/java/com/springboot/app/oauth/client/UsuarioFeignClient.java
@@ -0,0 +1,14 @@
+package com.springboot.app.oauth.client;
+
+import com.springboot.app.commons.usuarios.models.entity.Usuario;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name="servicio-usuarios")
+public interface UsuarioFeignClient {
+
+    @GetMapping("/usuarios/search/buscar-username")
+    Usuario findByUsername(@RequestParam String username);
+
+}
